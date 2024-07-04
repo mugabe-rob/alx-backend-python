df07306a465b45c53cbc8864e979747241118169
MUGABE Robertson <139157186+mugabe-rob@users.noreply.github.com>
2024-07-04 19:24:05 +0200

    Concatenating two strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-'''Task One's module.
+'''Task 1's module.
 '''
 
 
 def concat(str1: str, str2: str) -> str:
-    '''Concatenating the two strings.
+    '''Concatenates two strings.
     '''
     return str1 + str2
